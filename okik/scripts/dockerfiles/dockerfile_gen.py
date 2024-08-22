375fa9eac9e31a0e60cc484303849e2925b5f550
Akash Swamy <akashswamy@Mac.lan>
2024-08-22 14:42:14 +0100

    reverted uv for now

--- okik/scripts/dockerfiles/dockerfile_gen.py
+++ okik/scripts/dockerfiles/dockerfile_gen.py
@@ -42,26 +42,37 @@ CMD ["okik", "server", "-r", "-d"]
 
 dockerfile_content_uv_install = """
 # Use an official Python runtime as a parent image
-FROM python:3.10-slim
+FROM python:3.12-slim-bullseye
+
+# The installer requires curl (and certificates) to download the release archive
+RUN apt-get update && apt-get install -y --no-install-recommends curl ca-certificates git
+
+# Download the latest installer
+ADD https://astral.sh/uv/install.sh /uv-installer.sh
+
+# Run the installer then remove it
+RUN sh /uv-installer.sh && rm /uv-installer.sh
+
+# Ensure the installed binary is on the `PATH`
+ENV PATH="/root/.cargo/bin/:$PATH"
 
 # Set the working directory in the container
 WORKDIR /app
-
-# Install git and curl
-RUN apt-get update && apt-get install -y git curl
-
-# Install uv
-RUN pip install uv
-
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-# Install okik and its dependencies
-# create a virtual environment
-RUN uv venv
-RUN source .venv/bin/activate
-RUN uv init
-# activate the virtual environment
+# Sync the project into a new environment
+ENV UV_SYSTEM_PYTHON=1
+
+RUN uv venv /opt/venv
+# Use the virtual environment automatically
+ENV VIRTUAL_ENV=/opt/venv
+# Place entry points in the environment at the front of the path
+ENV PATH="/opt/venv/bin:$PATH"
+
+COPY . .
+RUN uv pip install -r requirements.txt
+
 # install okik
 RUN echo "Installing okik"
 RUN uv add okik
@@ -83,4 +94,4 @@ CMD ["okik", "server", "-r", "-d"]
 
 def create_dockerfile(path, name:str = "Dockerfile"):
     with open(f"{path}/{name}", 'w') as file:
-        file.write(dockerfile_content_uv_install)
+        file.write(dockerfile_content)
